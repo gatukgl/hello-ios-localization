93e8fccb738c1792edb35cfccf103cf6e0c047e2
Gatuk Chattanon <gatukgl@gmail.com>
2021-01-14 16:40:26 +0700

    Initial Commit

---
+++ hello-localization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello-localization
+//
+//  Created by Gatuk Chattanon on 14/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
